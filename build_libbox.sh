3905e9e0545c110e242f684e542eb2ec7141696b
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-09-22 18:25:25 +0800

    Update build_libbox.sh

--- build_libbox.sh
+++ build_libbox.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ##### 除非你知道自己在干什么，否则什么都不要动 #####
 # 想使用指定分支请修改链接，注意不要去掉后面的`sing-box`
-git clone https://github.com/SagerNet/sing-box sing-box
+git clone https://github.com/yaotthaha/sing-box-pub sing-box
 ###### !! 不要动 !! ######
 cd sing-box
 ##########################
 # 这里可以切换分支
 # git checkout dev-next-yaott
 ##########################
-# https://github.com/yaotthaha/sing-box-pub@dev-next-yaott 编译 with_clash_ui 需要，其他分支可以注释掉 #########
+https://github.com/yaotthaha/sing-box-pub@dev-next-yaott
 git submodule init
 git submodule update
 ####################
